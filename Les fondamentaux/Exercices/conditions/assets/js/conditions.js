3bd30faa8e7bf7586de1a627cf984598c380871e
3steak <fc.barcelone94@hotmail.fr>
2022-11-17 21:09:49 +0100

    Fin exercices conditions

--- Les fondamentaux/Exercices/conditions/assets/js/conditions.js
+++ Les fondamentaux/Exercices/conditions/assets/js/conditions.js
@@ -18,23 +18,74 @@ if (10 % num == 0) {
 
 // Exercice 3 
 
-let mark = 2;
+let mark = 4;
 
-switch (mark) {
-    case ((mark > 0) && (mark < 3)):
-        console.log("Mauvais");
+if ((mark >= 0) && (mark <= 3)) {
+    console.log('Mauvais');
+} else if ((mark >= 4) && (mark <= 5)) {
+    console.log('Moyen');
+} else if ((mark >= 6) && (mark <= 7)) {
+    console.log('Assez Bien');
+} else if ((mark >= 8) && (mark <= 9)) {
+    console.log('Bien');
+} else {
+    console.log('Excellent');
+};
+
+//  Avec SWITCH ( opérateurs de comparaison non valide)
+// switch (mark) {
+//     case 0:
+//     case 1:
+//     case 2:
+//     case 3:
+//         console.log("Mauvais");
+//         break;
+//     case 4:
+//     case 5:
+//         console.log("Moyen");
+//         break;
+//     case 6:
+//     case 7:
+//         console.log("Assez Bien");
+//         break;
+//     case 8:
+//     case 9:
+//         console.log("Bien");
+//         break;
+//     default:
+//         console.log("Excellent");
+// };
+
+// Exercice 4 
+let day = 4;
+
+switch (day) {
+
+    case 1:
+        console.log("Lundi");
         break;
-    case ((mark > 4) && (mark < 5)):
-        console.log("Moyen");
+    case 2:
+        console.log("Mardi");
         break;
-    case ((mark > 6) && (mark < 7)):
-        console.log("Assez Bien");
+    case 3:
+        console.log("Mercredi");
         break;
-    case ((mark > 8) && (mark < 9)):
-        console.log("Bien");
+    case 4:
+        console.log("Jeudi");
         break;
-    default:
-        console.log("Excellent");
+    case 5:
+        console.log("Ventredi");
+        break;
+    case 6:
+        console.log("Samedi");
+        break;
+    case 7:
+        console.log("Dimanche");
+        break;
 };
 
 
+// Exercice 5 
+
+let value = "oui";
+console.log('La valeur entrée est un(e) ' + typeof (value));
