c60ade09cdee8e3f37ab30b93b9c0e65748fb4e5
3steak <fc.barcelone94@hotmail.fr>
2022-11-19 16:42:48 +0100

    ajout exo selecteur

--- Les fondamentaux/Exercices/selecteurs/public/assets/js/script2.js
+++ Les fondamentaux/Exercices/selecteurs/public/assets/js/script2.js
@@ -1,6 +1,46 @@
 console.log('Exercice2');
 
-console.log(document.querySelector('#title'));
+console.log(document.querySelector('#title').innerText);
 
-console.log(document.querySelector('.description'));
+console.log(document.querySelector('.description').innerText);
+console.log(document.querySelector('ul').innerText);
+
+let textLi = document.querySelector('ul').innerText;
+
+
+// Exercice 3 
+console.log('Exercice 3');
+console.log(textLi);
+// J Utilise les backquotes pour modifier mon HTML 
+document.querySelector('ul').innerHTML = `<li>Javascript</li>
+<li>PHP</li>
+<li>...</li>`
+console.log(document.querySelector('ul').innerText);
+
+// Exercice 4 
+
+console.log('Exercice 4');
+
+
+console.log(document.querySelector('input'));
+console.log(document.querySelector('img'));
+//  Je stock le lien dans la var scrImg
+let scrImg = document.querySelector('img').getAttribute("src");
+console.log(scrImg);
+
+// je change la valeur de name dans l'input
+document.querySelector('input').setAttribute('name', scrImg);
+console.log(document.querySelector('input'));
+
+// Exercice 5 
+console.log('Exercice 5');
+// classList.add permet d'ajouter une class 
+document.querySelector('span').classList.add('important');
+console.log(document.getElementsByClassName('important'));
+
+// Exercice 6 
+console.log('Exercice 6');
+document.querySelector('li:first-of-type').classList.add('red');
+console.log(document.querySelector('li:first-of-type'));
+
 
