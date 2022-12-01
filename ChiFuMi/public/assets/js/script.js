a3f86a733c13e10232505bc80fc3214c2de75564
3steak <fc.barcelone94@hotmail.fr>
2022-12-01 15:36:53 +0100

    Connexion des deux jeux COUCOU YAPOU

--- ChiFuMi/public/assets/js/script.js
+++ ChiFuMi/public/assets/js/script.js
@@ -13,10 +13,14 @@ const rockNode = document.querySelector('.rock');
 const computeurChoiceNode = document.querySelector('.computeurChoice');
 const yourChoiceNode = document.querySelector('.yourChoice');
 const resultNode = document.querySelector('.result');
+const highScoreNode = document.querySelector('.hightScore');
 
 const paper = 0;
 const scissors = 1;
 const rock = 2;
+
+
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -24,22 +28,27 @@ function getRandomInt(max) {
 // mon tableau a donc pour index 0 la key paper etc.. 
 const choiceToText = ['Paper', 'Scissors', 'Rock'];
 
+
+
 paperNode.addEventListener('click', () => {
     // je définis ma variable qui me donnera un entier random
     let computeur = getRandomInt(3);
+
 
     // computeur vient jouer sur mon index du tableau
     //  il fait alors référence à paper scissors ou rock
     computeurChoiceNode.textContent = choiceToText[computeur];
     yourChoiceNode.textContent = choiceToText[paperNode.value];
 
+
+
     if (computeur === rock) {
-        resultNode.textContent = 'WINNER'
+        resultNode.textContent = 'TU CONTINUES ! ';
     } else if
         (computeur === paper) {
-        resultNode.textContent = " EGALITY"
+        resultNode.textContent = "EGALITE, REJOUONS !";
     } else {
-        resultNode.textContent = "LOOSER"
+        resultNode.textContent = "GAME OVER !";
     }
 })
 
@@ -51,12 +60,12 @@ scissorsNode.addEventListener('click', () => {
     computeurChoiceNode.textContent = choiceToText[computeur];
     yourChoiceNode.textContent = choiceToText[scissorsNode.value];
     if (computeur === rock) {
-        resultNode.textContent = "LOOSER"
+        resultNode.textContent = "GAME OVER !"
     } else if
         (computeur === paper) {
-        resultNode.textContent = 'WINNER'
+        resultNode.textContent = 'TU CONTINUES !'
     } else {
-        resultNode.textContent = " EGALITY"
+        resultNode.textContent = "EGALITE, REJOUONS !"
     }
 })
 
@@ -69,12 +78,12 @@ rockNode.addEventListener('click', () => {
     computeurChoiceNode.textContent = choiceToText[computeur];
     yourChoiceNode.textContent = choiceToText[rockNode.value];
     if (computeur === rock) {
-        resultNode.textContent = " EGALITY"
+        resultNode.textContent = "EGALITE, REJOUONS !"
     } else if
         (computeur === paper) {
-        resultNode.textContent = "LOOSER"
+        resultNode.textContent = "GAME OVER !"
     } else {
-        resultNode.textContent = 'WINNER'
+        resultNode.textContent = 'TU CONTINUES ! '
     }
 
 })
--- sandbox/assets/js/main.js
+++ sandbox/assets/js/main.js
@@ -1,7 +1,10 @@
+const game = document.querySelector('.game');
 const dino = document.getElementById('dino');
 const cactus = document.getElementById('cactus');
 const score = document.getElementById('score');
 const gameOver = document.getElementById('gameover');
+const startGame = document.getElementById('start');
+console.log(gameOver);
 // fonction jump qui ajoute ma class jump
 
 // ===============================================
@@ -46,6 +49,7 @@ let scoreCounter = () => {
 window.addEventListener("keydown", (start) => {
     // si code de la touche pressé == Space
     if (start.code == "Space") {
+        startGame.style.display = "none";
         gameOver.style.display = "none";
 
         // ajouter l'animation au cactus
@@ -74,7 +78,16 @@ let isAlive = setInterval(function () {
     if (cactusLeft < 30 && cactusLeft > 0 && dinoTop >= 140) {
         // collision
         // alert('gameover');
-        gameOver.style.display = "block";
+
+        //  SI CHIFUMI LOOOOSE
+        // setTimeout(function () {
+        //     gameOver.style.display = "none"
+        //     startGame.style.display = "block";
+        // }, 4000);
+        // gameOver.style.display = "block";
+        chifumi.style.display = "flex";
+        game.style.display = "none";
+
         cactus.style.animation = "none";
         clearInterval(intervalScore);
         playerScore = 0;
@@ -83,6 +96,102 @@ let isAlive = setInterval(function () {
 }, 10);
 
 
+// --------------------------------------------------------------
+
+//  CHIFUMI 
 
 
+
+const paperNode = document.querySelector('.paper');
+const scissorsNode = document.querySelector('.scissors');
+const rockNode = document.querySelector('.rock');
+const computeurChoiceNode = document.querySelector('.computeurChoice');
+const yourChoiceNode = document.querySelector('.yourChoice');
+const resultNode = document.querySelector('.result');
+const chifumi = document.querySelector('.chifumi');
+
+const paper = 0;
+const scissors = 1;
+const rock = 2;
+
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// mon tableau a donc pour index 0 la key paper etc.. 
+const choiceToText = ['Paper', 'Scissors', 'Rock'];
+
+
+
+paperNode.addEventListener('click', () => {
+    // je définis ma variable qui me donnera un entier random
+    let computeur = getRandomInt(3);
+
+
+    // computeur vient jouer sur mon index du tableau
+    //  il fait alors référence à paper scissors ou rock
+    computeurChoiceNode.textContent = choiceToText[computeur];
+    yourChoiceNode.textContent = choiceToText[paperNode.value];
+
+
+
+    if (computeur === rock) {
+        resultNode.textContent = 'TU CONTINUES ! ';
+        setTimeout(function () {
+            chifumi.style.display = "none";
+            game.style.display = "block";
+        }, 5000);
+    } else if
+        (computeur === paper) {
+        resultNode.textContent = "EGALITE, REJOUONS !";
+    } else {
+        resultNode.textContent = "GAME OVER !";
+    }
+})
+
+
+scissorsNode.addEventListener('click', () => {
+    let computeur = getRandomInt(3);
+    // computeur vient jouer sur mon index du tableau
+    //  il fait alors référence à paper scissors ou rock
+    computeurChoiceNode.textContent = choiceToText[computeur];
+    yourChoiceNode.textContent = choiceToText[scissorsNode.value];
+    if (computeur === rock) {
+        resultNode.textContent = "GAME OVER !"
+    } else if
+        (computeur === paper) {
+        resultNode.textContent = 'TU CONTINUES !'
+        setTimeout(function () {
+            chifumi.style.display = "none";
+            game.style.display = "block";
+        }, 5000);
+    } else {
+        resultNode.textContent = "EGALITE, REJOUONS !"
+    }
+})
+
+
+rockNode.addEventListener('click', () => {
+    let computeur = getRandomInt(3);
+
+    // computeur vient jouer sur mon index du tableau
+    //  il fait alors référence à paper scissors ou rock
+    computeurChoiceNode.textContent = choiceToText[computeur];
+    yourChoiceNode.textContent = choiceToText[rockNode.value];
+    if (computeur === rock) {
+        resultNode.textContent = "EGALITE, REJOUONS !"
+    } else if
+        (computeur === paper) {
+        resultNode.textContent = "GAME OVER !"
+    } else {
+        resultNode.textContent = 'TU CONTINUES ! '
+        setTimeout(function () {
+            chifumi.style.display = "none";
+            game.style.display = "block";
+        }, 5000);
+    }
+
+})
 
