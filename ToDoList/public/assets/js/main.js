d465d38e021896b4e34cc95aa1d1d95e8ea41176
3steak <fc.barcelone94@hotmail.fr>
2022-12-13 19:50:06 +0100

    oui

--- ToDoList/public/assets/js/main.js
+++ ToDoList/public/assets/js/main.js
@@ -5,6 +5,7 @@
 const formTasks = document.getElementById('formTasks');
 const inputAddTask = document.getElementById('inputAddTask');
 const buttonAddTask = document.getElementById('buttonAddTask');
+const buttonsRemoveTask = document.getElementsByClassName('trash');
 const checkbox = document.getElementById('checkbox');
 const inputList = document.getElementById('inputList');
 const lists = document.getElementById('lists');
@@ -14,6 +15,14 @@ const lists = document.getElementById('lists');
 
 let toDoList = [];
 
+//  let toDoList = {
+//     name: '',
+//     archived: false,
+//     checked: false,
+// }
+let toArray = (item) => {
+    return Array.from(item)
+}
 
 // Ajout d'une tâche
 let addTask = buttonAddTask.addEventListener('click', (event) => {
@@ -21,28 +30,41 @@ let addTask = buttonAddTask.addEventListener('click', (event) => {
         alert("Ajoute une tâche");
     }
     else {
-        toDoList.unshift(inputAddTask.value);
+        toDoList.unshift({
+            name: inputAddTask.value,
+            archived: false,
+            checked: false,
+        });
         // je clear le DOM 
         lists.innerHTML = ``;
         toDoList.forEach((element, index) => {
             lists.innerHTML += `
         <div class=" input-group my-4 " id="list${index}">
-        <span class="input-group-text"><input type="checkbox" name="checkbox" id="checkbox"></span>
-        <input type="text" class="form-control" id="inputList" value='`+ element + `'  readonly disabled >
-        <span class="input-group-text" id="trash${index}"><i class="fa-solid fa-trash" ></i></span>
-        <span class="input-group-text" id="archive${index}"><i class="fa-solid fa-box-archive archive"></i></span>
-    </div>`;
+            <span class="input-group-text"><input type="checkbox" name="checkbox" id="checkbox"></span>
+            <input type="text" class="form-control" id="inputList" value='`+ element.name + `'  readonly disabled >
+            <span class="input-group-text trash" value="${index}" id="trash${index}"><i class="fa-solid fa-trash" ></i></span>
+            <span class="input-group-text" id="archive${index}"><i class="fa-solid fa-box-archive archive"></i></span>
+        </div>`;
+            updateRemoveListeners()
+
+            // test archived 
+            toDoList.forEach((element) => { return element.archived = true })
+            console.log(toDoList)
         });
-        console.log(toDoList);
-    }
+
+        // let buttonsRemoveTaskArray = toArray(buttonsRemoveTask);
+    };
 });
 
-// if (addTask) {
 
-//     for (let index = 0; index < toDoList.length; index++) {
-//         console.log(toDoList[index]);
-//     }
-// }
-
-
+const updateRemoveListeners = () => {
+    toArray(buttonsRemoveTask).forEach((trashButton) => {
+        if (trashButton) {
+            trashButton.addEventListener('click', (e) => {
+                delete toDoList[trashButton.getAttribute('value')];
+                document.getElementById("list" + trashButton.getAttribute('value')).remove()
+            })
+        }
+    })
+}
 
